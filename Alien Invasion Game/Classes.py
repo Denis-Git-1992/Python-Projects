5c17cae03df6d974323c24bcd40d346a61426c8d
Denis <dentoc1@gmail.com>
2025-07-21 15:27:16 +0300

    Commit

---
+++ Alien Invasion Game/Classes.py
@@ -0,0 +1,2 @@
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
--- Alien Invasion Game/alien_invasion.py
+++ Alien Invasion Game/alien_invasion.py
@@ -1,3 +1,37 @@
-import sys
-import pygame
+class Car:
 
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{self.year}, {self.make}, {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading } miles on it")
+
+    def update_odometer(self, mileage):
+        if mileage < self.odometer_reading:
+            print("You can not roll back odometer")
+        else:
+            self.odometer_reading = mileage
+
+    def increment_odometer(self, miles):
+        if miles > 0:
+            self.odometer_reading += miles
+        else:
+            print("Please, enter a valid number of miles")
+
+my_new_car  = Car("audi", "a4", 2024)
+
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+my_new_car.update_odometer(15)
+my_new_car.read_odometer()
+my_new_car.update_odometer(14)
+my_new_car.read_odometer()
+my_new_car.increment_odometer(289)
+my_new_car.read_odometer()
