6d7f2ac196d7e0e0f6337dbad8c0dbb4a6282f1a
Denis <dentoc1@gmail.com>
2025-07-29 15:10:40 +0300

    Commit

--- Alien Invasion Game/alien_invasion.py
+++ Alien Invasion Game/alien_invasion.py
@@ -8,7 +8,7 @@ class AlienInvasion:
         """Initialize the game and create game resources"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1100, 800))
+        self.screen = pygame.display.set_mode((1200, 700))
         pygame.display.set_caption("Alien Invasion")
         # Set background color
         self.bg_color = (230, 230, 230)
